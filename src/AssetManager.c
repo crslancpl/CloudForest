6dc2ee2d82ffb1510f8a8d004c1b9c9b44098a39
ericNen <ericnien1223@gmail.com>
2025-07-07 16:48:14 +0800

    update ui

--- src/AssetManager.c
+++ src/AssetManager.c
@@ -1,1 +1,15 @@
-#include "AssetLoader.h"
+#include "AssetManager.h"
+
+GResource *UIResources;
+
+bool LoadUIRes(){
+    GError e;
+    UIResources = g_resource_load("gresources/ui.gresource.xml", &e);
+    if(e.code != 0){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+g_resources_register(GResource *resource)
