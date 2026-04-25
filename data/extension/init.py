d0bf73f446a75c64e93855b4c6286bc1f9eab2e6
ericNen <ericnien1223@gmail.com>
2026-04-25 09:20:12 +0800

    disable extension loading

--- data/extension/init.py
+++ data/extension/init.py
@@ -10,6 +10,7 @@ currentdirectory = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdirectory)
 sys.path.insert(0, parentdir)
 
+"""
 with open("data/extension/enabled.json") as enabled_json:
     enabled_extensions = json.load(enabled_json)
     for extension_name in enabled_extensions:
@@ -17,3 +18,4 @@ with open("data/extension/enabled.json") as enabled_json:
         print()
         with open(extension["location"]) as entry:
             exec(entry.read())
+"""
